4281ab9c1aacc898c2c9a05901c15fc9b7e3a5d5
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2025-03-14 16:08:03 +0400

    dashboard count linking

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,52 +7,60 @@
             <div class="col-lg-12">
                 <div class="row gutters-10">
                     <div class="col-3">
-                        <div class="bg-grad-6 text-white rounded-lg mb-4 overflow-hidden">
-                            <div class="px-3 pt-3">
-                                <div class="fs-20">
-                                    <span class=" d-block">{{  trans('messages.total') }}</span>
-                                    Categories
+                        <a href="{{ route('categories.index') }}" style="color:white;">
+                            <div class="bg-grad-6 text-white rounded-lg mb-4 overflow-hidden">
+                                
+                                <div class="px-3 pt-3">
+                                    <div class="fs-20">
+                                        <span class=" d-block">{{  trans('messages.total') }}</span>
+                                        Categories
+                                    </div>
+                                    <div class="h3 fw-700 mb-3">{{ App\Models\Category::count() }}</div>
                                 </div>
-                                <div class="h3 fw-700 mb-3">{{ App\Models\Category::count() }}</div>
+                                
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                                    <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                                        d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                                    </path>
+                                </svg>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
-                                    d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
-                                </path>
-                            </svg>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-3">
-                        <div class="bg-grad-3 text-white rounded-lg mb-4 overflow-hidden">
-                            <div class="px-3 pt-3">
-                                <div class="fs-20">
-                                    <span class="d-block">{{  trans('messages.total') }}</span>
-                                    Industries
+                        <a href="{{ route('industries.index') }}" style="color:white;">
+                            <div class="bg-grad-3 text-white rounded-lg mb-4 overflow-hidden">
+                                <div class="px-3 pt-3">
+                                    <div class="fs-20">
+                                        <span class="d-block">{{  trans('messages.total') }}</span>
+                                        Industries
+                                    </div>
+                                    <div class="h3 fw-700 mb-3">{{ App\Models\Industry::count() }}</div>
                                 </div>
-                                <div class="h3 fw-700 mb-3">{{ App\Models\Industry::count() }}</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                                    <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                                        d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                                    </path>
+                                </svg>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
-                                    d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
-                                </path>
-                            </svg>
-                        </div>
+                        </a>
                     </div>
                     <div class="col-3">
-                        <div class="bg-grad-1 text-white rounded-lg mb-4 overflow-hidden">
-                            <div class="px-3 pt-3">
-                                <div class="fs-20">
-                                    <span class=" d-block">{{  trans('messages.total') }}</span>
-                                    Technologies
+                        <a href="{{ route('technologies.index') }}" style="color:white;">
+                            <div class="bg-grad-1 text-white rounded-lg mb-4 overflow-hidden">
+                                <div class="px-3 pt-3">
+                                    <div class="fs-20">
+                                        <span class=" d-block">{{  trans('messages.total') }}</span>
+                                        Technologies
+                                    </div>
+                                    <div class="h3 fw-700 mb-3">{{ App\Models\Technology::count() }}</div>
                                 </div>
-                                <div class="h3 fw-700 mb-3">{{ App\Models\Technology::count() }}</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                                    <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                                        d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                                    </path>
+                                </svg>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
-                                    d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
-                                </path>
-                            </svg>
-                        </div>
+                        </a>
                     </div>
                     {{-- <div class="col-3">
                         <div class="bg-grad-4 text-white rounded-lg mb-4 overflow-hidden">
@@ -71,20 +79,22 @@
                         </div>
                     </div> --}}
                     <div class="col-3">
-                        <div class="bg-grad-5 text-white rounded-lg mb-4 overflow-hidden">
-                            <div class="px-3 pt-3">
-                                <div class="fs-20">
-                                    <span class=" d-block">{{  trans('messages.total') }}</span>
-                                    Portfolios
+                        <a href="{{ route('portfolios.index') }}" style="color:white;">
+                            <div class="bg-grad-5 text-white rounded-lg mb-4 overflow-hidden">
+                                <div class="px-3 pt-3">
+                                    <div class="fs-20">
+                                        <span class=" d-block">{{  trans('messages.total') }}</span>
+                                        Portfolios
+                                    </div>
+                                    <div class="h3 fw-700 mb-3">{{ App\Models\Portfolio::count() }}</div>
                                 </div>
-                                <div class="h3 fw-700 mb-3">{{ App\Models\Portfolio::count() }}</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                                    <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                                        d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                                    </path>
+                                </svg>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
-                                    d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
-                                </path>
-                            </svg>
-                        </div>
+                        </a>
                     </div>
                     {{-- <div class="col-3">
                         <div class="bg-grad-2 text-white rounded-lg mb-4 overflow-hidden">
@@ -240,8 +250,8 @@
                                 datasets: [{
                                     label: 'Portfolio Count by Categories',
                                     data: data,
-                                    backgroundColor: 'rgb(90, 166, 228, 0.6)',
-                                    borderColor: 'rgb(90, 166, 228, 1)',
+                                    backgroundColor: 'rgba(247, 95, 95, 0.6)',
+                                    borderColor: 'rgb(247, 95, 95, 1)',
                                     borderWidth: 1
                                 }]
                             },
@@ -285,8 +295,8 @@
                                 datasets: [{
                                     label: 'Portfolio Count by Industries',
                                     data: dataInd,
-                                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                                    borderColor: 'rgba(54, 162, 235, 1)',
+                                    backgroundColor: 'rgb(90, 166, 228, 0.6)',
+                                    borderColor: 'rgb(90, 166, 228, 1)',
                                     borderWidth: 1
                                 }]
                             },
